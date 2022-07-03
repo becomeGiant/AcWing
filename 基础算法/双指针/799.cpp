8a03ba9fa12c43698e2e03fe5aa5649802957e61
becomeGiant <98026098+becomeGiant@users.noreply.github.com>
2022-07-03 16:04:43 +0800

    Create 799.cpp

---
+++ 基础算法/双指针/799.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=1e5+10;
+int n;
+int a[N];
+int state[N];
+
+int main()
+{
+    cin>>n;
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    int ans=0;
+    for(int i=0,j=0;i<n;i++)
+    {
+        state[a[i]]++;
+        while(j<n&&state[a[i]]>1)
+        {
+            state[a[j]]--;
+            j++;
+        }
+        ans=max(ans,i-j+1);
+    }
+    cout<<ans;
+}
