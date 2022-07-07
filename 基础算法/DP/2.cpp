d904d4efb519af546f44fde4b686d88ae9d6f051
becomeGiant <98026098+becomeGiant@users.noreply.github.com>
2022-07-07 19:25:51 +0800

    Update 2.cpp

--- 基础算法/DP/2.cpp
+++ 基础算法/DP/2.cpp
@@ -1,1 +1,25 @@
+01背包问题思路：
+状态表示(集合)：所有选法--->满足只从前i个物品中选，且总体积<=j
+状态转移(集合划分：从小状态(集合)到大状态(集合))：包含i(曲线救国，先剔除i，从不包含i的角度出发，再加上i)与不包含i
 
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=1010;
+int n,m;
+int v[N],w[N];
+int f[N][N];
+
+int main()
+{
+    cin>>n>>m;
+    for(int i=1;i<=n;i++)
+        cin>>v[i]>>w[i];
+    for(int i=1;i<=n;i++)
+        for(int j=0;j<=m;j++)
+        {
+            f[i][j]=f[i-1][j];
+            if(j>=v[i]) f[i][j]=max(f[i][j],f[i-1][j-v[i]]+w[i]);
+        }
+    cout<<f[n][m];
+}
