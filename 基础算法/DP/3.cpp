ddbccb7adf112dee5924d86bb942b59b4b612514
becomeGiant <98026098+becomeGiant@users.noreply.github.com>
2022-07-07 22:06:43 +0800

    Update 3.cpp

--- 基础算法/DP/3.cpp
+++ 基础算法/DP/3.cpp
@@ -25,3 +25,29 @@ int main()
     cout<<f[n][m];
 }
 
+
+
+优化后：
+优化思路：去除一个k循环。把f[i][j]的状态展开，寻找子状态(f[i][j-v]+w)进行替代优化
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=1010;
+int n,m;
+int v[N],w[N];
+int f[N][N];
+
+int main()
+{
+    cin>>n>>m;
+    for(int i=1;i<=n;i++)
+        cin>>v[i]>>w[i];
+    for(int i=1;i<=n;i++)
+        for(int j=0;j<=m;j++)
+        {
+            f[i][j]=f[i-1][j];
+            if(j>=v[i])
+                f[i][j]=max(f[i][j],f[i][j-v[i]]+w[i]);
+        }
+    cout<<f[n][m];
+}
