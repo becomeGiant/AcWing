4123b65a3281757892538bfafb196230ca550c09
becomeGiant <98026098+becomeGiant@users.noreply.github.com>
2022-07-16 18:46:20 +0800

    Update 844.cpp

--- 基础算法/图论/BFS/844.cpp
+++ 基础算法/图论/BFS/844.cpp
@@ -1,1 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+const int N=110;
+int n,m;
+int graph[N][N],dist[N][N];
+queue<pair<int,int>> q;
+
+void bfs()
+{
+    fill(dist[0],dist[0]+N*N,-1);
+    dist[0][0]=0;
+    pair<int,int> cur={0,0};
+    q.push(cur);
+    int dx[]={0,0,-1,1},dy[]={1,-1,0,0};
+    while(!q.empty())
+    {
+        cur=q.front();    //取队头
+        q.pop();
+        for(int i=0;i<4;i++)
+        {
+            int nextx=cur.first+dx[i],nexty=cur.second+dy[i];
+            if(nextx>=0&&nextx<n&&nexty>=0&&nexty<m&&graph[nextx][nexty]==0&&dist[nextx][nexty]==-1)
+            {
+                dist[nextx][nexty]=dist[cur.first][cur.second]+1;
+                q.push({nextx,nexty});    //拓展队头
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin>>n>>m;
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+            cin>>graph[i][j];
+    bfs();
+    cout<<dist[n-1][m-1];
+}
