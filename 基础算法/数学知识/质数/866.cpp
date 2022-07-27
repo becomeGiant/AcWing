3d1fd4921fab5d8895c038af050730ac3476580d
becomeGiant <98026098+becomeGiant@users.noreply.github.com>
2022-07-27 22:02:36 +0800

    Update 866.cpp

--- 基础算法/数学知识/质数/866.cpp
+++ 基础算法/数学知识/质数/866.cpp
@@ -1,1 +1,30 @@
+1.约数是成对存在的
+  d是n的约数,那么n/d也是n的约数
+  如果从小到大依次枚举,那么只需要枚举满足d≤n/d的数
 
+#include<bits/stdc++.h>
+using namespace std;
+
+bool prime(int n)
+{
+    if(n<=1)
+        return false;
+    for(int i=2;i<=n/i;i++)
+        if(n%i==0)
+            return false;
+    return true;
+}
+
+int main()
+{
+    int n,m;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>m;
+        if(prime(m))
+            cout<<"Yes"<<endl;
+        else
+            cout<<"No"<<endl;
+    }
+}
