d1d75be3858b5c577c306fd0b1a16967c037c638
becomeGiant <98026098+becomeGiant@users.noreply.github.com>
2022-07-28 11:23:56 +0800

    Update 867.cpp

--- 基础算法/数学知识/质数/867.cpp
+++ 基础算法/数学知识/质数/867.cpp
@@ -1,5 +1,7 @@
 分解质因数(短除法分解)
 如果其中某个质因数多次出现,则可以用幂次的形式表示
+当试除数是i时,表示已经把2~i-1中的所有质因子除完
+例如i=3时,表示n一定不含2
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -8,7 +10,7 @@ void divide(int n)
 {
     for(int i=2;i<=n/i;i++)
     {
-        if(n%i==0)  //i是质因数
+        if(n%i==0)  //i一定是质数
         {
             int sum=0;
             while(n%i==0)
