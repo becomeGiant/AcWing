913f50a3450565407b4ec126b8c6966dafc5fac1
becomeGiant <98026098+becomeGiant@users.noreply.github.com>
2022-07-28 21:19:39 +0800

    Update 869.cpp

--- 基础算法/数学知识/约数/869.cpp
+++ 基础算法/数学知识/约数/869.cpp
@@ -1,1 +1,35 @@
+试除法求约数
+1.从小到大依次枚举所有数
+  优化的算法同"试除法求质数"
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int n,m;
+set<int> res;
+
+void divisor(int x)
+{
+    res.clear();
+    for(int i=1;i<=x/i;i++)
+    {
+        if(x%i==0)
+        {
+            res.insert(i);
+            res.insert(x/i);
+        }
+    }
+    for(auto i:res)
+        cout<<i<<" ";
+    cout<<endl;
+}
+
+int main()
+{
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>m;
+        divisor(m);
+    }
+}
