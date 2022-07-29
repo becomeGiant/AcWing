8b0af1c53b04b967628ccd7b83419bc6c77883f5
becomeGiant <98026098+becomeGiant@users.noreply.github.com>
2022-07-29 09:32:37 +0800

    Update 870.cpp

--- 基础算法/数学知识/约数/870.cpp
+++ 基础算法/数学知识/约数/870.cpp
@@ -5,3 +5,42 @@
     由唯一分解定理,一个数的所有约数也是质数,所以这些约数也可以使用幂次表示
     M=P1的B1次幂*P2的B2次幂*...*Pk的Bk次幂(约数,唯一分解定理),且0≤Bi≤Ai
     
+#include<bits/stdc++.h>
+using namespace std;
+
+const int mod=1e9+7;
+map<int,int> primes;
+
+void divide(int x)
+{
+    for(int i=2;i<=x/i;i++)
+    {
+        if(x%i==0)
+        {
+            int sum=0;
+            while(x%i==0)
+            {
+                x/=i;
+                sum++;
+            }
+            primes[i]+=sum;
+        }
+    }
+    if(x>1)
+        primes[x]+=1;
+}
+
+int main()
+{
+    int n,m;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>m;
+        divide(m);
+    }
+    long long res=1;
+    for(auto i:primes)
+        res=res*(i.second+1)%mod;
+    cout<<res;
+}
