384574ebc8cd0518f26b8b2fd2b71ec1bad1967f
becomeGiant <98026098+becomeGiant@users.noreply.github.com>
2022-07-30 16:07:08 +0800

    Update 868_2.cpp

--- 基础算法/数学知识/质数/868_2.cpp
+++ 基础算法/数学知识/质数/868_2.cpp
@@ -1,9 +1,10 @@
-原理：n只会被最小质因子筛除
+原理：n只会被它的最小质因子筛除
   primes[]是存放质数的
-  i%primes[j]==0
+  1.i%primes[j]==0
     primes[j]一定是i的最小质因子,primes[j]一定是primes[j]*i的最小质因子
-  i%primes[j]!=0
+  2.i%primes[j]!=0
     primes[j]一定小于i的所有质因子,primes[j]一定是primes[j]*i的最小质因子
+  3.对于任意一个合数x,假设primes[j]是x的最小质因子,当i枚举到x/primes[j]的时候,x一定会在state[primes[j]*i]被筛除
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -23,7 +24,7 @@ int main()
         for(int j=0;primes[j]<=n/i;j++)
         {
             state[primes[j]*i]=true;
-            if(i%primes[j]==0)  //primes[j]是最小质因子
+            if(i%primes[j]==0)  //primes[j]是最小质因子,当出现最小质因子的时候退出
                 break;
         }
     }
