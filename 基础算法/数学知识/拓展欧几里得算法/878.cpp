a1ac3a57b0a9ac26b6e558bcea05acf1ec502b15
becomeGiant <98026098+becomeGiant@users.noreply.github.com>
2022-08-02 18:31:56 +0800

    Update 878.cpp

--- 基础算法/数学知识/拓展欧几里得算法/878.cpp
+++ 基础算法/数学知识/拓展欧几里得算法/878.cpp
@@ -1,1 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int exgcd(int a,int b,int &x,int &y)
+{
+    if(b==0)
+    {
+        x=1,y=0;
+        return a;
+    }
+    int d=exgcd(b,a%b,y,x);
+    y=y-a/b*x;
+    return d;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int a,b,m,x,y;
+        cin>>a>>b>>m;
+        int d=exgcd(a,m,x,y);
+        if(b%d==0)  cout<<(long long)x*(b/d)%m<<endl;
+        else cout<<"impossible"<<endl;
+    }
+}
