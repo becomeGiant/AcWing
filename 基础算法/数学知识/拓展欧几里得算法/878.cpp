7b0b73102c953fd8f9b4a0dd5bad8e2ceb287ee5
becomeGiant <98026098+becomeGiant@users.noreply.github.com>
2022-08-03 12:21:12 +0800

    Update 878.cpp

--- 基础算法/数学知识/拓展欧几里得算法/878.cpp
+++ 基础算法/数学知识/拓展欧几里得算法/878.cpp
@@ -1,9 +1,10 @@
 拓展欧几里得算法 的应用
+真实方程 推得 目标公式
 
-裴属定理：ax+by=d
-目标公式：ax%m=b
-  ax+(-my)=b (y=ax/m)
-  
+欧几里得：(a,b)=(b,a%b)=d
+裴属定理：ax+by=d  真实方程
+题目：ax%m=b  目标公式
+  ax=my+b (y=ax/m)
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -15,8 +16,10 @@ int exgcd(int a,int b,int &x,int &y)
         x=1,y=0;
         return a;
     }
-    int d=exgcd(b,a%b,y,x);
-    y=y-a/b*x;
+    int d=exgcd(b,a%b,x,y);
+    int temp=x;
+    x=y;
+    y=temp-a/b*y;
     return d;
 }
 
@@ -26,10 +29,12 @@ int main()
     cin>>n;
     while(n--)
     {
-        int a,b,m,x,y;
+        int a,b,x,y,m;
         cin>>a>>b>>m;
         int d=exgcd(a,m,x,y);
-        if(b%d==0)  cout<<(long long)x*(b/d)%m<<endl;
-        else cout<<"impossible"<<endl;
+        if(b%d==0)
+            cout<<(long long)x*(b/d)%m<<endl;   //方程两边同乘b/d
+        else
+            cout<<"impossible"<<endl;
     }
 }
