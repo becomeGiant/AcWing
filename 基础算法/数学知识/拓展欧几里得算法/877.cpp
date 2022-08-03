69aebaf527f3164122c89c38d721051c6428bc87
becomeGiant <98026098+becomeGiant@users.noreply.github.com>
2022-08-03 11:48:45 +0800

    Update 877.cpp

--- 基础算法/数学知识/拓展欧几里得算法/877.cpp
+++ 基础算法/数学知识/拓展欧几里得算法/877.cpp
@@ -2,9 +2,9 @@
 
 列出两个公式可以推出结果
 欧几里得：(a,b)=(b,a%b)=d
-裴属定理：ax+by=d
+裴属定理：ax+by=d  目标公式
     bx+(a%b)y=d
-    ay+b(x-a/b*y)=d
+    ay+b(x-a/b*y)=d 真实方程
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -20,7 +20,7 @@ int exgcd(int a,int b,int &x,int &y)
     int temp=x;
     x=y;
     y=temp-a/b*y;
-    cout<<"x:"<<x<<" y:"<<y<<endl;
+    // cout<<"x:"<<x<<" y:"<<y<<endl;
     return d;
 }
 
